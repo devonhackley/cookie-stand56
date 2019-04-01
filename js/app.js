3499c783f5f1d715f9feff0fd1c51b95e0ccf344
Devon Hackley <devonhackley@gmail.com>
2019-04-01 15:30:35 -0700

    was able to get sales to render correctly

--- js/app.js
+++ js/app.js
@@ -27,37 +27,258 @@ const pike = {
         this.calcCookiesSales();
 
         //attaching store name
-        var h2 = document.createElement('h2');
+        const h2 = document.createElement('h2');
         h2.textContent = this.name;
-        var main = document.getElementById('main-area');
+        const main = document.getElementById('main-area');
         main.appendChild(h2);
 
         //attaching li to list
-        var ul = document.createElement('ul');
+        const ul = document.createElement('ul');
         hours.forEach((element, index) => {
-            var li = document.createElement('li');
+            const li = document.createElement('li');
             li.textContent = element + ': ' + this.dailySales[index] + ' cookies';
             ul.appendChild(li);
         })
 
         //attaching total sales to daily sales
-        var totalSales = 0;
+        let totalSales = 0;
         this.dailySales.forEach((ele) => {
             totalSales += ele;
         })
-        var li = document.createElement('li');
+        const li = document.createElement('li');
         li.textContent = 'Total Sales: ' + totalSales + ' cookies';
         ul.appendChild(li);
 
         //Append ul to the dom
         main.appendChild(ul);
+
+        // Append a line break
+        const lb = document.createElement('br');
+        main.appendChild(lb);
     }
 
 };
-const airport = {};
-const center = {};
-const hill = {};
-const alki = {};
+const airport = {
+    name: 'SeaTac Airport',
+    minHourlyCust: 3,
+    maxHourlyCust: 24,
+    avgCookiePerSale: 1.2,
+    dailySales: [],
 
-// create pike store
+    // function that generates a random number of custormers
+    randomNumCustomer: function (){
+        return Math.floor(Math.random() * (this.maxHourlyCust - this.minHourlyCust) + this.minHourlyCust);
+    },
+
+    // function that calculates the number of cookies per hour based on a random num
+    calcCookiesSales: function (){
+        this.dailySales = hours.map(() => {
+            return Math.floor(this.randomNumCustomer() * this.avgCookiePerSale)
+        })
+    },
+
+    renderSales: function() {
+        // calculate sales
+        this.calcCookiesSales();
+
+        //attaching store name
+        const h2 = document.createElement('h2');
+        h2.textContent = this.name;
+        const main = document.getElementById('main-area');
+        main.appendChild(h2);
+
+        //attaching li to list
+        const ul = document.createElement('ul');
+        hours.forEach((element, index) => {
+            const li = document.createElement('li');
+            li.textContent = element + ': ' + this.dailySales[index] + ' cookies';
+            ul.appendChild(li);
+        })
+
+        //attaching total sales to daily sales
+        let totalSales = 0;
+        this.dailySales.forEach((ele) => {
+            totalSales += ele;
+        })
+        const li = document.createElement('li');
+        li.textContent = 'Total Sales: ' + totalSales + ' cookies';
+        ul.appendChild(li);
+
+        //Append ul to the dom
+        main.appendChild(ul);
+
+        // Append a line break
+        const lb = document.createElement('br');
+        main.appendChild(lb);
+    }
+};
+const center = {
+    name: 'Seattle Center',
+    minHourlyCust: 11,
+    maxHourlyCust: 38,
+    avgCookiePerSale: 3.7,
+    dailySales: [],
+
+    // function that generates a random number of custormers
+    randomNumCustomer: function (){
+        return Math.floor(Math.random() * (this.maxHourlyCust - this.minHourlyCust) + this.minHourlyCust);
+    },
+
+    // function that calculates the number of cookies per hour based on a random num
+    calcCookiesSales: function (){
+        this.dailySales = hours.map(() => {
+            return Math.floor(this.randomNumCustomer() * this.avgCookiePerSale)
+        })
+    },
+
+    renderSales: function() {
+        // calculate sales
+        this.calcCookiesSales();
+
+        //attaching store name
+        const h2 = document.createElement('h2');
+        h2.textContent = this.name;
+        const main = document.getElementById('main-area');
+        main.appendChild(h2);
+
+        //attaching li to list
+        const ul = document.createElement('ul');
+        hours.forEach((element, index) => {
+            const li = document.createElement('li');
+            li.textContent = element + ': ' + this.dailySales[index] + ' cookies';
+            ul.appendChild(li);
+        })
+
+        //attaching total sales to daily sales
+        let totalSales = 0;
+        this.dailySales.forEach((ele) => {
+            totalSales += ele;
+        })
+        const li = document.createElement('li');
+        li.textContent = 'Total Sales: ' + totalSales + ' cookies';
+        ul.appendChild(li);
+
+        //Append ul to the dom
+        main.appendChild(ul);
+
+        // Append a line break
+        const lb = document.createElement('br');
+        main.appendChild(lb);
+    }
+};
+const hill = {
+    name: 'Capitol Hill',
+    minHourlyCust: 20,
+    maxHourlyCust: 38,
+    avgCookiePerSale: 2.3,
+    dailySales: [],
+
+    // function that generates a random number of custormers
+    randomNumCustomer: function (){
+        return Math.floor(Math.random() * (this.maxHourlyCust - this.minHourlyCust) + this.minHourlyCust);
+    },
+
+    // function that calculates the number of cookies per hour based on a random num
+    calcCookiesSales: function (){
+        this.dailySales = hours.map(() => {
+            return Math.floor(this.randomNumCustomer() * this.avgCookiePerSale)
+        })
+    },
+
+    renderSales: function() {
+        // calculate sales
+        this.calcCookiesSales();
+
+        //attaching store name
+        const h2 = document.createElement('h2');
+        h2.textContent = this.name;
+        const main = document.getElementById('main-area');
+        main.appendChild(h2);
+
+        //attaching li to list
+        const ul = document.createElement('ul');
+        hours.forEach((element, index) => {
+            const li = document.createElement('li');
+            li.textContent = element + ': ' + this.dailySales[index] + ' cookies';
+            ul.appendChild(li);
+        })
+
+        //attaching total sales to daily sales
+        let totalSales = 0;
+        this.dailySales.forEach((ele) => {
+            totalSales += ele;
+        })
+        const li = document.createElement('li');
+        li.textContent = 'Total Sales: ' + totalSales + ' cookies';
+        ul.appendChild(li);
+
+        //Append ul to the dom
+        main.appendChild(ul);
+
+        // Append a line break
+        const lb = document.createElement('br');
+        main.appendChild(lb);
+    }
+};
+const alki = {
+    name: 'Alki',
+    minHourlyCust: 2,
+    maxHourlyCust: 16,
+    avgCookiePerSale: 4.6,
+    dailySales: [],
+
+    // function that generates a random number of custormers
+    randomNumCustomer: function (){
+        return Math.floor(Math.random() * (this.maxHourlyCust - this.minHourlyCust) + this.minHourlyCust);
+    },
+
+    // function that calculates the number of cookies per hour based on a random num
+    calcCookiesSales: function (){
+        this.dailySales = hours.map(() => {
+            return Math.floor(this.randomNumCustomer() * this.avgCookiePerSale)
+        })
+    },
+
+    renderSales: function() {
+        // calculate sales
+        this.calcCookiesSales();
+
+        //attaching store name
+        const h2 = document.createElement('h2');
+        h2.textContent = this.name;
+        const main = document.getElementById('main-area');
+        main.appendChild(h2);
+
+        //attaching li to list
+        const ul = document.createElement('ul');
+        hours.forEach((element, index) => {
+            const li = document.createElement('li');
+            li.textContent = element + ': ' + this.dailySales[index] + ' cookies';
+            ul.appendChild(li);
+        })
+
+        //attaching total sales to daily sales
+        let totalSales = 0;
+        this.dailySales.forEach((ele) => {
+            totalSales += ele;
+        })
+        const li = document.createElement('li');
+        li.textContent = 'Total Sales: ' + totalSales + ' cookies';
+        ul.appendChild(li);
+
+        //Append ul to the dom
+        main.appendChild(ul);
+
+        // Append a line break
+        const lb = document.createElement('br');
+        main.appendChild(lb);
+        
+    }
+};
+
+// create stores
 pike.renderSales();
+airport.renderSales();
+center.renderSales();
+hill.renderSales();
+alki.renderSales();
